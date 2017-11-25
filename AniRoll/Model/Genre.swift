1b5d94704121b2f1a786eed0df9f23cdd68319a6
Lech H. Conde <lech@mavels.net>
2017-11-24 19:48:51 -0600

    Added AlamofireImage

---
+++ AniRoll/Model/Genre.swift
@@ -0,0 +1,13 @@
+//
+//  Genre.swift
+//  AniRoll
+//
+//  Created by Lech H. Conde on 11/24/17.
+//  Copyright © 2017 Lech H. Conde. All rights reserved.
+//
+
+import UIKit
+
+class Genre: Object {
+
+}
